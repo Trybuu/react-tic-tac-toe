2e7cebf5dfcb963ddcc3944840ed0141f56e26f6
Marek Trybuła <trybu001@gmail.com>
2023-11-13 19:14:11 +0100

    Player view and logic - made , GameBoard view - made

---
+++ src/components/Player.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+export function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName)
+
+  const [isEditing, setIsEditing] = useState(false)
+
+  function handleEditClick() {
+    setIsEditing((editing) => !isEditing)
+  }
+
+  function handleChange(event) {
+    console.log(event)
+    setPlayerName(event.target.value)
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        value={playerName}
+        onChange={handleChange}
+        required
+      ></input>
+    )
+    // btnCaption = 'Save'
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  )
+}
